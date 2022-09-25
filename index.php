9e7832e3d8ed54257d835cecce7aeeaab6e6e58f
Marcos Mitsi <marcos.mitsi@hotmail.com>
2022-09-24 22:43:39 -0300

    Criando formulário

---
+++ index.php
@@ -0,0 +1,64 @@
+
+<!--Templete utilizado https://colorlib.com/wp/template/login-form-v8/-->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Usando Biblioteca PHPMailer</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/css/util.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form p-l-55 p-r-55 p-t-178">
+					<span class="login100-form-title">
+                    Enviar Mesagem <br>
+                    <span class="txt1 p-b-9">
+						É rápido e fácil.
+						</span>
+					</span>
+
+                    <div class="wrap-input100 validate-input m-b-16">
+						<input class="input100" type="text" name="nome" placeholder="Nome Completo">
+						<span class="focus-input100"></span>
+					</div>
+
+                    <div class="wrap-input100 validate-input m-b-16">
+						<input class="input100" type="mail" name="email" placeholder="E-mail">
+						<span class="focus-input100"></span>
+					</div>
+                    
+
+					<div class="wrap-input100 validate-input m-b-16">
+						<input class="input100" type="text" name="assunto" placeholder="Assunto da Mensagem">
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input m-b-16">
+                    <textarea class="input100" id="mensg" rows="10" placeholder="Mensagem"></textarea>
+						
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Enviar
+						</button>
+					</div><br>
+
+					
+				</form>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
