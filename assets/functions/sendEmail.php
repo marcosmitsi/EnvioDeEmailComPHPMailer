d462fcd318465d5e674c9e141197e9f9cfadd4b1
Marcos Mitsi <marcos.mitsi@hotmail.com>
2022-09-26 23:01:13 -0300

    enviando email ao cliente

---
+++ assets/functions/sendEmail.php
@@ -0,0 +1,52 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Carregando Composer autoloader
+require '../lib/vendor/autoload.php';
+
+$dadosForm = $_POST;
+
+if(isset($dadosForm['SendAddMsg'])){
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.titan.email';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'no-reply@mitsi.com.br';
+    $mail->Password   = '&rDM22!9@7x7';
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+
+    //Envia email ao Cliente
+    //Remetente
+    $mail->setFrom('no-reply@mitsi.com.br', 'Marcos Mitsi'); //Remetente
+    $mail->addReplyTo('no-reply@mitsi.com.br', 'Marcos Mitsi'); // responder para
+
+    //Destinatário
+    $mail->addAddress($dadosForm['email'], $dadosForm['nome']);     //Add a recipient
+
+    //Configuração do email
+    $mail->isHTML(true); // Aceita HTML
+    $mail->Subject =utf8_decode("Mesagem de contato recebida");
+    $body = utf8_decode("Prezado(a) " . $dadosForm['nome'] . "<br><br>
+    Recebemos sua mensagem. <br> Será lido o mais rápido possível.<br> Em breve será respondido.<br>
+    Assunto: " . $dadosForm['assunto'] . "<br> Conteúdo: " . $dadosForm['mensg']);
+
+    $mail->Body    = $body;
+    //$mail->AltBody = utf8_decode($dadosForm['mensg'] . "Enviado Via PHPMailer.\n");
+    $mail->send();
+    echo 'Email Enviado';
+   
+
+    
+} catch (Exception $e) {
+    echo "A mensagem não pôde ser enviada. Erro de correspondência: {$mail->ErrorInfo}" . $e->getMessage();
+}
+}
--- index.php
+++ index.php
@@ -1,64 +1,68 @@
-
 <!--Templete utilizado https://colorlib.com/wp/template/login-form-v8/-->
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
 	<title>Usando Biblioteca PHPMailer</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="assets/css/util.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 </head>
+
 <body>
-	
+
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
-				<form class="login100-form validate-form p-l-55 p-r-55 p-t-178">
+
+				
+				<form class="login100-form validate-form p-l-55 p-r-55 p-t-178" name="msg" action="assets/functions/sendEmail.php" method="POST">
 					<span class="login100-form-title">
-                    Enviar Mesagem <br>
-                    <span class="txt1 p-b-9">
-						É rápido e fácil.
+						Enviar Mesagem <br>
+						<span class="txt1 p-b-9">
+							É rápido e fácil.
 						</span>
 					</span>
 
-                    <div class="wrap-input100 validate-input m-b-16">
-						<input class="input100" type="text" name="nome" placeholder="Nome Completo">
-						<span class="focus-input100"></span>
-					</div>
-
-                    <div class="wrap-input100 validate-input m-b-16">
-						<input class="input100" type="mail" name="email" placeholder="E-mail">
-						<span class="focus-input100"></span>
-					</div>
-                    
-
 					<div class="wrap-input100 validate-input m-b-16">
-						<input class="input100" type="text" name="assunto" placeholder="Assunto da Mensagem">
+						<input class="input100" type="text" name="nome" id="nome" placeholder="Nome Completo">
 						<span class="focus-input100"></span>
 					</div>
 
 					<div class="wrap-input100 validate-input m-b-16">
-                    <textarea class="input100" id="mensg" rows="10" placeholder="Mensagem"></textarea>
-						
+						<input class="input100" type="email" name="email" id="email" placeholder="E-mail">
 						<span class="focus-input100"></span>
 					</div>
 
-					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
+
+					<div class="wrap-input100 validate-input m-b-16">
+						<input class="input100" type="text" name="assunto" id="assunto" placeholder="Assunto da Mensagem">
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input m-b-16">
+						<textarea class="input100" id="mensg" name="mensg" rows="10" placeholder="Mensagem"></textarea>
+
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="container-100-form-btn">
+						<button class="login100-form-btn" name="SendAddMsg" type="submit" value="Enviar">
 							Enviar
 						</button>
 					</div><br>
 
-					
+
 				</form>
 			</div>
 		</div>
 	</div>
 
 </body>
+
 </html>
